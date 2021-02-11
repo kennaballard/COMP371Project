72fca1fa22b2bbf9197be2923522dd90dbad52a1
kennedy <kennaballard19@gmail.com>
2021-02-11 02:15:30 -0500

    Camera class setup
    - Experimented with panning and tilting
    - Designed Camera class
    - Apply camera to world matrix
    - TODO: input & pan/tilt gradually

--- src/models/alphanumericals/ModelKennedy.cpp
+++ src/models/alphanumericals/ModelKennedy.cpp
@@ -1,15 +1,6 @@
 #pragma once
 #include <iostream>
 #include "Model.h"
-#define GLEW_STATIC 1   // This allows linking with Static Library on Windows, without DLL
-#include <GL/glew.h>    // Include GLEW - OpenGL Extension Wrangler
-
-#include <GLFW/glfw3.h> // GLFW provides a cross-platform interface for creating a graphical context,
-// initializing OpenGL and binding inputs
-
-#include <glm/glm.hpp>  // GLM is an optimized math library with syntax to similar to OpenGL Shading Language
-#include <glm/gtc/matrix_transform.hpp>
-
 #include "../models/alphanumericals/Model0.cpp"
 #include "../models/alphanumericals/Model4.cpp"
 #include "../models/alphanumericals/ModelK.cpp"
